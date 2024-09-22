ff3e1967060fe4563982b66f79be3219c8021caa
Alina P. <apukhaevaa@gmail.com>
2024-09-22 19:51:19 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,18 @@
+# Пухаева Алина Александровна
+# Homework for the lesson "Ways to call a function"
+# 22.09.2024
+# Notes: ctrl+alt+L
+def send_email(message, recipient, *, sender):
+    standard_senders = ['university.help@gmail.com', 'urban.teacher@mail.ru']
+    if sender == recipient:
+        print(f'Нельзя отправить письмо самому себе!')
+        return
+    if sender not in standard_senders:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Напоминание', 'vasyok1337@gmail.com', sender='university.help@gmail.com')
+send_email('Новое письмо', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Не отправляется', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
